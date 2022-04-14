32cb8eb5cdf517c9f19ad47de4ef69b930309038
LuisFernando1496 <luisfernando_orantes@outlook.com>
2022-04-14 15:54:27 -0500

    nombre de almacen

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -4,13 +4,20 @@
 <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
     <div class="container">
         <a class="navbar-brand">
-            @if(Auth::user())
-                {{Auth::user()->branchOffice->name}}
-            @else
+            @if (Auth::user())
+                @if (Auth::user()->rol_id != 4)
+                    {{ Auth::user()->branchOffice->name }}
+                @else
+                    @if (Auth::user()->rol_id == 4)
+                        Almacen TJGreens
+                    @else
+                    @endif
+                @endif
                 {{ config('app.name', 'Laravel') }}
             @endif
         </a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
             <span class="navbar-toggler-icon"></span>
         </button>
 
@@ -34,15 +41,15 @@
                         </a>
                     </li>
                     <li class="nav-item dropdown">
-                        <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                        <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button"
+                            data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                             {{ Auth::user()->name }}
                         </a>
 
                         <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
                             <a class="dropdown-item" href="perfil">Perfil</a>
-                            <a class="dropdown-item" href="{{ route('logout') }}"
-                                onclick="event.preventDefault();
-                                                document.getElementById('logout-form').submit();">
+                            <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
+                                                    document.getElementById('logout-form').submit();">
                                 {{ __('Cerrar sesión') }}
                             </a>
 
@@ -56,5 +63,3 @@
         </div>
     </div>
 </nav>
-
-
