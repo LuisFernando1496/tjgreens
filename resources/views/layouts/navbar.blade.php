6b156206a24ea9f74d2bf6c5c34bde98d5cd6057
LuisFernando1496 <luisfernando_orantes@outlook.com>
2022-04-15 12:23:59 -0500

    nombre de almacen

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -11,8 +11,11 @@
                     @if (Auth::user()->rol_id == 4)
                         Almacen TJGreens
                     @else
+                    {{ config('app.name', 'Laravel') }}
                     @endif
+                   
                 @endif
+                @else
                 {{ config('app.name', 'Laravel') }}
             @endif
         </a>
@@ -49,7 +52,7 @@
                         <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
                             <a class="dropdown-item" href="perfil">Perfil</a>
                             <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
-                                                    document.getElementById('logout-form').submit();">
+                                                        document.getElementById('logout-form').submit();">
                                 {{ __('Cerrar sesión') }}
                             </a>
 
