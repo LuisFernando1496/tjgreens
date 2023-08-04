4659d3d74065db2d2839ea8b39acd4ce6aaed3f3
Ventitux-Fernando <fernandos.ventitux@gmail.com>
2023-08-04 15:10:13 -0600

    prueba

--- app/Exports/inventarioAlmacen.php
+++ app/Exports/inventarioAlmacen.php
@@ -7,7 +7,7 @@ use App\BranchOffice;
 use Maatwebsite\Excel\Concerns\FromView;
 use Illuminate\Contracts\View\View;
 class inventarioAlmacen implements FromView
-{
+{ 
     private $dataGlobal;
     public function __construct($option)
     {
